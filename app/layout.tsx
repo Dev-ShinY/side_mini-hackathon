06d6abd78052e2d9e2d3b64bc24918614a07402c
shinyunsung <114058540+Dev-ShinY@users.noreply.github.com>
2022-12-15 17:44:03 +0900

    Revert ":sparkles: Fix server components"
    This reverts commit 33ea75597403da2cfe12746c37d020e09660c703.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,33 @@
+"use client";
+
 import "../styles/globals.css";
+import { darkTheme, lightTheme } from "../styles/theme-style";
+import { ThemeProvider } from "styled-components";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { darkModeVar } from "../src/utils/apollo";
+import { useReactiveVar } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../src/utils/apollo";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const darkMode = useReactiveVar(darkModeVar);
+
   return (
     <html>
+      <head />
       <body>
-        <Header />
-        {children}
-        <Footer />
+        <ApolloProvider client={client}>
+          <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+            <Header />
+            {children}
+            <Footer />
+          </ThemeProvider>
+        </ApolloProvider>
       </body>
     </html>
   );
